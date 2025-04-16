a382f522ea28ece38e7691c71c2893a0f399081c
Frijidia <espoir.agoli-agbo@epitech.eu>
2025-04-16 11:13:31 +0100

    mentions legals page

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Index />} /> */}
-          <Route path="/" element={<Contact />} />
+          <Route path="/" element={<Legal />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/portfolio" element={<Portfolio />} />
