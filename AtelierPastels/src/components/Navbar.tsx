04bf96cb30ae724fdd25fc9067738ddd6ada6b06
Frijidia <espoir.agoli-agbo@epitech.eu>
2025-04-17 14:48:13 +0100

    adding white navbar

--- AtelierPastels/src/components/Navbar.tsx
+++ AtelierPastels/src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`w-full z-50 py-4 shadow-md ${getNavbarBg(location.pathname)}`}>
+    <nav className={`w-full z-50 py-4 shadow-md `}> {/* ${getNavbarBg(location.pathname)}`>*/}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <div className="flex-shrink-0">
